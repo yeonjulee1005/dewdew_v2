dc6d3a6342078fed1ba1e3889be4e39c2bd96c57
Yeonju Lee <yeonju.lee1005@gmail.com>
2022-11-27 11:25:29 +0900

    dewdew 페이지 Version: 1.1.3.b - pwa 테스트

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,6 +48,7 @@ export default {
   },
   // modules
   modules: [
+    '@kevinmarrec/nuxt-pwa',
     '@nuxtjs/robots',
     '@pinia/nuxt',
     '@vueuse/nuxt'
@@ -75,5 +76,26 @@ export default {
   // vueuse
   vueuse: {
     ssrHandlers: false
+  },
+  pwa: {
+    meta: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0',
+      name: 'Dewdew',
+      author: 'Dewdew',
+      description: '안녕하세요. FE 개발자 이연주입니다.',
+      theme_color: '#705757',
+      lang: 'ko',
+      // Generate splash screens for iOS
+      mobileAppIOS: true
+    },
+    manifest: {
+      name: 'Dewdew',
+      lang: 'ko',
+      useWebmanifestExtension: false
+    },
+    workbox: {
+      enabled: true
+    }
   }
 }
