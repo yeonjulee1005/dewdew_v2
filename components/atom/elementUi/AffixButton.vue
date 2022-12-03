427fa5e1935e72b2e687224173ef7eb176adf67e
Yeonju Lee <yeonju.lee1005@gmail.com>
2022-12-03 19:40:31 +0900

    dewdew페이지 Version: 1.2.1 - useWindowScroll() 사용 - 블로그 위로가기 버튼 생성 - 블로그 좋아요 플로팅 버튼 생성 - tiptap 텍스트 높이 수정

---
+++ components/atom/elementUi/AffixButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-affix
+    :class="{'el-affix-on': affixButtonProps.trigger}"
+    :position="affixButtonProps.position"
+    :offset="affixButtonProps.offset"
+  >
+    <el-button
+      circle
+      size="large"
+      :type="affixButtonProps.activateLike ? 'danger' : ''"
+      @click="clickAffixButton"
+    >
+      {{ affixButtonProps.buttonText }}
+    </el-button>
+  </el-affix>
+</template>
+<script setup lang="ts">
+const affixButtonProps = defineProps({
+  trigger: { type: Boolean, default: false },
+  position: { type: String, default: 'bottom' },
+  offset: { type: Number, default: 200 },
+  buttonText: { type: String, default: '❤️' },
+  activateLike: { type: Boolean, default: false }
+})
+
+const affixButtonEmits = defineEmits([
+  'click-affix'
+])
+
+const clickAffixButton = () => {
+  affixButtonEmits('click-affix')
+}
+
+</script>
