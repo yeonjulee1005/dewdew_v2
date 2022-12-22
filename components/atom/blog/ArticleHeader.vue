0ab553f84d1a823e0c77ea50881046d13306be84
Yeonju Lee <yeonju.lee1005@gmail.com>
2022-12-22 16:55:44 +0900

    🫶dewdew페이지 Version: 1.2.4.a - 블로그 타이틀 스타일 수정

--- components/atom/blog/ArticleHeader.vue
+++ components/atom/blog/ArticleHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="article-header flex flex-column flex-space-between flex-align-end mb-20">
+  <div class="article-header flex flex-column flex-space-between mb-default">
     <div class="title">
       {{ articleHeaderProps.data.title }}
     </div>
-    <div class="time">
+    <div class="time flex flex-justify-end mt-default">
       {{ articleHeaderProps.data.createdAt }}
     </div>
   </div>
