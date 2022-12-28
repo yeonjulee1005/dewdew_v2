67b60483ffcf7055628eef113653f82e8cacc265
Yeonju Lee <yeonju.lee1005@gmail.com>
2022-12-28 17:06:35 +0900

    🚜dewdew 페이지 Version: 1.2.13.b - app.vue 로딩로직 수정

--- app.vue
+++ app.vue
@@ -25,10 +25,11 @@ useHead({
   }
 })
 
-await useDatabase().updateCoreData(await useApi().getSsrCoreData('core', 'images'))
-if (typeof useDatabase().coreData.value === 'string') {
-  await useDatabase().updateCoreData(await useApi().getClientCoreData('core', 'images'))
-}
+useDatabase().updateCoreData(await useApi().getSsrCoreData('core', 'images'))
+
+// if (typeof useDatabase().coreData.value === 'string') {
+//   await useDatabase().updateCoreData(await useApi().getClientCoreData('core', 'images'))
+// }
 
 // 아래는 set으로 컬랙션 추가하는거
 // await useApi().postSetData('blog', 'article', { desc: '내용3', index: 0, title: '제11목이댱' })
